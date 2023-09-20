71f1585fc0f78c4087827845124ed1f07e0ddeda
sukwenruan <sukwenru@hawaii.edu>
2023-09-20 12:46:12 -1000

    9/20

--- Lab5/lab5.js
+++ Lab5/lab5.js
@@ -8,6 +8,6 @@ month_of_birth = 8;
 let calc1 = age + fav_num / day_of_birth * month_of_birth
 let calc2 = (age + fav_num) / day_of_birth * month_of_birth
 
-//output calc 1  & 2 to the DOM
+//output calc 1 & 2 to the DOM
 document.getElementById("result1").innerHTML = calc1;
 document.getElementById("result2").innerHTML = calc2;
