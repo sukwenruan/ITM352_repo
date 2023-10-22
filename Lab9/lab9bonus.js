f6bf1d5a02b5514b73e0ace748ff7a0721dd4976
sukwenruan <sukwenru@hawaii.edu>
2023-10-21 20:31:30 -1000

    lab9

--- Lab9/lab9bonus.js
+++ Lab9/lab9bonus.js
@@ -1,11 +1,17 @@
-function calculateTax(monthly_sales, tax_rate) {
-    if(Number(tax_rate) !=tax_rate) {
-        errors.push('Invalid tax rate.');
-    }
-    if (!Array.isArray(monthly_sales)) {
-        errors.push('Invalid input. Monthly sales must be an array of numbers.');
-    }
-    const tax_owing = monthly_sales.map(sales => sales*tax_rate);
-    return tax_owing;
-}
+//array of numbers and tax_rate for inputs
+let monthly_sales = [100, 200, 300, 400];
+let tax_rate = 0.04; //4.00% tax
 
+//function to calculate tax owing for each month's sales
+function calculateTax(monthly_sales, tax) {
+    let tax_owing = [];
+    for (let i = 0; i < monthly_sales.length; i++) {
+        tax_owing.push(monthly_sales[i]*tax);
+    }
+    return tax_owing;
+};
+
+let tax_owing = calculateTax(monthly_sales, tax_rate);
+
+//outputs in console
+console.log(tax_owing);
