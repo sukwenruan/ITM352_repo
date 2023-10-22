e6f1bb4a548ac65098d0974117a5c267dfd03361
sukwenruan <sukwenru@hawaii.edu>
2023-10-21 21:24:59 -1000

    POKE10

---
+++ POKEs/POKE10/Invoice3/products.js
@@ -0,0 +1,32 @@
+//let quantity = [2,1,1,3,12];
+let quantity = [2,'cat',-0.1,-3,0.12];
+
+let itemData = [
+    {
+        brand: 'Gillette Sensor 3 Razor',
+        price: 1.23,
+        quantityIndex: 0,
+    },
+    {
+        brand: 'Barbasol Shaving Cream',
+        price: 2.64,
+        quantityIndex: 1,
+    },
+    {
+        brand: 'Nautica Cologne',
+        price: 6.17,
+        quantityIndex: 2,
+    },
+    {
+        brand: 'Rubbing Alcohol',
+        price: 0.98,
+        quantityIndex: 3,
+    },
+    {
+        brand: 'Colgate Classic Toothbrush',
+        price: 1.89,
+        quantityIndex: 4,
+    },
+];
+
+export { itemData, quantity };
