bfc78e25c7795ba422df8701b3bb86df4073503c
sukwenruan <sukwenru@hawaii.edu>
2023-10-22 14:14:33 -1000

    lab11

---
+++ Lab11/display_purchase.js
@@ -0,0 +1,6 @@
+//orders4.js
+let params= (new URL(document.location)).searchParams;
+let quantity = params.get("qty_textbox");
+
+let message = `Thank you for ordering ${quantity} things!`;
+document.body.innerHTML = message;
